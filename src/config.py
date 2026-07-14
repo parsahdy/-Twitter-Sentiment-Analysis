7bf887c6b925d50b5ba02b96b36acc9e1956d614
unknown <pheydari33@yahoo.com>
2026-07-14 10:52:34 +0330

    Config file created for restoring parameters and file directories

---
+++ src/config.py
@@ -0,0 +1,7 @@
+MAX_FEATURES = 10000
+TEST_SIZE = 0.2
+RANDOM_STATE = 42
+FILE_PATH=r"D:\python\Sentiment140\data\processed\cleaned_sentiment140.csv"
+CHECKPOINT="distilbert-base-uncased"
+TRANSFORMER_MODEL_PATH="models/transformer_model.pkl"
+TRANSFORMER_TOKENIZER_PATH="models/transformer_tokenizer.pkl"
