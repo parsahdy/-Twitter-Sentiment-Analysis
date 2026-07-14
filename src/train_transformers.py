77892d7da02b591712d968fa2ac0c64e2708a1fa
unknown <pheydari33@yahoo.com>
2026-07-14 16:08:56 +0330

    Add transformer model training and evaluation

--- src/train_transformers.py
+++ src/train_transformers.py
@@ -1,20 +1,19 @@
 import mlflow
 import mlflow.transformers
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer, TrainingArguments
+
 from config import CHECKPOINT, TRANSFORMER_MODEL_PATH, TRANSFORMER_TOKENIZER_PATH, FILE_PATH
 from data import load_data, train_test_split_data
-from transformers_utils import build_test_dataset, compute_metrics
+from transformers_utils import build_train_test_dataset, compute_metrics, tokenizer
 
 model_path = TRANSFORMER_MODEL_PATH
-tokenizer_path = TRANSFORMER_TOKENIZER_PATH
 
 model = AutoModelForSequenceClassification.from_pretrained(model_path)
-tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
 
 df = load_data(FILE_PATH)
 _, X_test, _, y_test = train_test_split_data(df)
 
-test_dataset = build_test_dataset(X_test, y_test, tokenizer)
+train_dataset, test_dataset = build_train_test_dataset(X_test, y_test, tokenizer)
 
 mlflow.set_experiment("sentiment140_transformers")
 
@@ -26,8 +25,9 @@ with mlflow.start_run():
             per_device_eval_batch_size=16,
             report_to="none"
         ),
+        train_dataset=train_dataset,
         eval_dataset=test_dataset,
-        tokenizer=tokenizer,
+        processing_class=tokenizer,
         compute_metrics=compute_metrics,
     )
 
