618044288e4f94d82a4a89663fec91e511df9ae9
unknown <pheydari33@yahoo.com>
2026-07-15 21:08:48 +0330

    general updates

--- src/train_transformers.py
+++ src/train_transformers.py
@@ -7,13 +7,15 @@ from data import load_data, train_test_split_data
 from transformers_utils import build_train_test_dataset, compute_metrics, tokenizer
 
 model_path = TRANSFORMER_MODEL_PATH
+tokenizer_path = TRANSFORMER_TOKENIZER_PATH
 
 model = AutoModelForSequenceClassification.from_pretrained(model_path)
 
 df = load_data(FILE_PATH)
-_, X_test, _, y_test = train_test_split_data(df)
+X_train, X_test, y_train, y_test = train_test_split_data(df, text_col="Clean_Text",
+                                                        lable_col="Label")
 
-train_dataset, test_dataset = build_train_test_dataset(X_test, y_test, tokenizer)
+train_dataset, test_dataset = build_train_test_dataset(X_train, X_test, y_train, y_test, tokenizer)
 
 mlflow.set_experiment("sentiment140_transformers")
 
@@ -27,20 +29,23 @@ with mlflow.start_run():
         ),
         train_dataset=train_dataset,
         eval_dataset=test_dataset,
-        processing_class=tokenizer,
+        tokenizer=tokenizer,
         compute_metrics=compute_metrics,
     )
 
     metrics = trainer.evaluate()
 
     params = {
-        "model": model,
-        "tokenizer": tokenizer,
         "checkpoint": CHECKPOINT,
+        "model_name": model_path,
+        "tokenizer_name": tokenizer_path,
     }
 
-    mlflow.log_param(params)
-    mlflow.log_metrics(metrics)
+    for k, v in params.items():
+        mlflow.log_param(k, str(v))
+    
+    for k, v in metrics.items():
+        mlflow.log_metrics(k, float(v))
 
     mlflow.transformers.log_model(
         transformers_model={"model": model, "tokenizer": tokenizer},
